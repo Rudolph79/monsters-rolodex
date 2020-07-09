42f92df01263929de195458b6f210e0bf2b95dc1
EBANG MEZUI ME MVE <rudolph.ebang@gmail.com>
2020-07-09 17:47:17 +0200

    Updates

---
+++ precache-manifest.c66e4893f1d9099d7a2ac25c68e50f33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07f097f129c47688250c83163ed52c3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4c678cef0c3bc54d4f9c",
+    "url": "/monsters-rolodex/static/css/main.78a54900.chunk.css"
+  },
+  {
+    "revision": "8eefb47c5e7c6f1166f6",
+    "url": "/monsters-rolodex/static/js/2.27033893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.27033893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c678cef0c3bc54d4f9c",
+    "url": "/monsters-rolodex/static/js/main.174ebeba.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
